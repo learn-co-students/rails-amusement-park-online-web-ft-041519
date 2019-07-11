26a4b312bd73050451e0f9efe59ac6ec7bfdb05c
jpier2012 <jpier2012@gmail.com>
2019-07-10 20:06:30 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,18 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        if user.tickets >= attraction.tickets
+            if user.height < attraction.min_height
+                "Sorry. You are not tall enough to ride the #{attraction.name}."
+            else
+                user.tickets -= attraction.tickets
+                user.nausea += attraction.nausea_rating
+                user.happiness += attraction.happiness_rating
+            end
+        else
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+        end        
+    end
 end
---
+++ db/migrate/20190711024414_ride.rb
@@ -0,0 +1,4 @@
+class Ride < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
