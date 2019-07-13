7c23c6c602509178f5938d53736f32a7a1be3a5f
Erin Stalzer <erinstalzer@gmail.com>
2019-07-13 16:30:37 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
-class User < ActiveRecord::Base
+class User < ActiveRecord::Base 
+    has_many :rides
+    has_many :attractions, through: :rides
+    has_secure_password
+    
+    def initialize(value)
+        #self.admin ||= false
+    end
+
+    def mood
+        if self.nauseous > self.happiness
+           "sad"
+        else
+            "happy"
+        end
+    end
 end
