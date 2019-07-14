f87248f603511ce96ec7bc874af6003dd5532123
Erin Stalzer <erinstalzer@gmail.com>
2019-07-13 23:54:30 -0400

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -7,7 +7,7 @@ class RidesController < ApplicationController
         ride = Ride.find_by(id: params["id"])
         user = User.find_by(id: ride.user_id)
         attraction = Attraction.find_by(id: ride.attraction_id)
-        @message = ride.take_ride
-        redirect_to user_path(user, message: @message)
+        session[:message] = ride.take_ride
+        redirect_to user_path(user)
     end
 end
