8e623738877a6b89389e19d2e00653be318d207d
Erin Stalzer <erinstalzer@gmail.com>
2019-07-13 20:40:28 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,7 +3,19 @@ class Ride < ActiveRecord::Base
     belongs_to :user
 
     def take_ride
-        
+        if self.tickets > @user.tickets && elf.min_height > @user.height 
+            "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
+        elsif self.tickets > @user.tickets
+            "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
+            redirect_to user_path(@user)
+        elsif self.min_height > @user.height 
+            "Sorry. You are not tall enough to ride the #{@attraction.name}."
+        else
+            @user.tickets -= self.tickets
+            @user.nausea -= self.nausea
+            @user.happiness
+        end
+
     end
 
 end
